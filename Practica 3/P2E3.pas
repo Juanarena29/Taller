fa7e8effc70af4374e715459f7ce8af2d3705d4a
Juanarena29 <151331182+Juanarena29@users.noreply.github.com>
2024-09-02 23:30:30 -0300

    Create P2E3.pas

---
+++ Practica 3/P2E3.pas
@@ -0,0 +1,14 @@
+Program P2E3;
+type
+  dias = 1..31;
+  meses = 1..12;
+  anios = 2020..2024;
+  
+  fechas = record
+    dia : dias;
+    mes : meses;
+    anio : anios;
+    end;
+  
+  alumnos = record
+    

3cd9e8a3168ee33d8d6c351e312f02dcc44dc4d6
Juanarena29 <151331182+Juanarena29@users.noreply.github.com>
2024-09-21 20:46:46 -0300

    Update Ejercicio1.java

--- NETBEANS/Ejercicio1.java
+++ NETBEANS/Ejercicio1.java
@@ -3,23 +3,22 @@ package tema1;
 import PaqueteLectura.GeneradorAleatorio;
 
 public class Ej01Tabla2 {
+
+ 
+    
     public static void main(String[] args) {
-        GeneradorAleatorio random = new GeneradorAleatorio();
+        GeneradorAleatorio.iniciar();
         int DF=11;  
         int [] tabla2 = new int[DF]; // indices de 0 a 10
         int i;
-        for (i=0;i<DF;i++) //cargar el vector
+        for (i=0;i<DF;i++) 
             tabla2[i]=2*i;
-        
-        int NumAleatorio;
-        do {
-            //generar num aleatorio entre 0 y 10
-          NumAleatorio = random.generarInt(12); //generarInt es el "metodo" de Generador automatico
-          if (NumAleatorio != 11)
-            System.out.println("2x"+NumAleatorio+"="+tabla2[NumAleatorio]); //muestro el resultado accediendo al vector
-        } while (NumAleatorio != 10);
-        
+        int NumAl=GeneradorAleatorio.generarInt(12);
+        while (NumAl != 11) {
+            System.out.println("2x" + NumAl + "="+ tabla2[NumAl]);
+            NumAl=GeneradorAleatorio.generarInt(12);
+        }
+    }
 
     }
     
-}

55db6b0c852f20e2e7327aac5eeca1af43d8f6c2
Juanarena29 <151331182+Juanarena29@users.noreply.github.com>
2024-09-29 11:37:35 -0300

    Update Ejercicio4.java

--- NETBEANS/Ejercicio4.java
+++ NETBEANS/Ejercicio4.java
@@ -1,21 +1,39 @@
 package tema1;
 
-import  PaqueteLectura.GeneradorAleatorio;
+import PaqueteLectura.GeneradorAleatorio;
 
-public class Demo07For {
+import PaqueteLectura.Lector;
 
+public class cuatro {
+   
     public static void main(String[] args) {
-       GeneradorAleatorio.iniciar();
-       int [][] matriz = new int [8][4];
-       int i;
-       int j;
-       for (i=0;i<7;i++) //LLENAR VECTOR
-           for (j=0;j<4;j++)
-               matriz[i][j] = GeneradorAleatorio.generarInt(10);
-       
-        
-         }
-
-    }
-    
+        GeneradorAleatorio.iniciar(); 
+        int [][] matriz = new int[8][4];
+        int piso;
+        int ofi;
+        int i, j;
+        for (i=0;i<8;i++)
+            for (j=0;j<4;j++)
+                matriz[i][j]=0;
+        System.out.println("Piso: (1..8) ");
+        piso = Lector.leerInt();
+        while (piso != 9) {
+            ofi = GeneradorAleatorio.generarInt(4);
+            matriz[piso-1][ofi] = matriz[piso-1][ofi] + 1;
+            System.out.println("Piso: (1..8) ");
+            piso = Lector.leerInt();  
+        }
+        for (i=0;i<8;i++)
+            for (j=0;j<4;j++) {
+                System.out.println("En el piso "+(i+1)+" - oficina "+(j+1)+" Concurrieron "+matriz[i][j]+" personas");
+                
+            }
+                
+            
+    } 
 }
+    
+    
+    
+
+
